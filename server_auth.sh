80be7b73c1bcd1883b716bf61e63bcd3dc16eda8
reyluar18 <reydelcastilloluarjr@gmail.com>
2024-05-14 06:34:35 +0800

    fix

--- server_auth.sh
+++ server_auth.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . /etc/openvpn/login/config.sh
 
-USERNAME="$1"
-PASSWORD="$2"
+USERNAME="$PAM_USER"
+read PASSWORD
 
 QUERY="SELECT user_name FROM users WHERE user_name='$USERNAME' AND auth_vpn=md5('$PASSWORD') AND status='live' AND is_freeze=0 AND is_ban=0 AND (duration > 0 OR vip_duration > 0 OR private_duration > 0)"
 RESULT=`mysql -u $USER -p$PASS -D $DB -h $HOST -sN -e "$QUERY"`
