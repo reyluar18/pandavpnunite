f13b9cf9c9908299314318935ca9604a3edf234e
reyluar18 <reydelcastilloluarjr@gmail.com>
2024-05-13 13:56:20 +0800

    wrong dropbear port

--- all_in_one.sh
+++ all_in_one.sh
@@ -87,7 +87,7 @@ rm -f /etc/motd
 touch /etc/motd.tail
 
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i "s|DROPBEAR_PORT=22|DROPBEAR_PORT=$PORT_WEBSOCKET|g" /etc/default/dropbear
+sed -i "s|DROPBEAR_PORT=22|DROPBEAR_PORT=$PORT_DROPBEAR|g" /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 service dropbear restart
