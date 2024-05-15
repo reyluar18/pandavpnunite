3109811977bba92af12575caff427a2ca0ea368b
reyluar18 <reydelcastilloluarjr@gmail.com>
2024-05-15 21:01:14 +0800

    add panda_aio

--- server_auth.sh
+++ server_auth.sh
@@ -5,18 +5,20 @@ USERNAME="$PAM_USER"
 read PASSWORD
 
 QUERY="SELECT user_name FROM users WHERE user_name='$USERNAME' AND auth_vpn=md5('$PASSWORD') AND status='live' AND is_freeze=0 AND is_ban=0 AND (duration > 0 OR vip_duration > 0 OR private_duration > 0)"
+echo $QUERY >> /etc/authorization/pandavpnunite/auth.log
 RESULT=`mysql -u $USER -p$PASS -D $DB -h $HOST -sN -e "$QUERY"`
 if [ "$RESULT" != '' ] && [ "$RESULT" = "$USERNAME" ]; then 
-    echo "user : $USERNAME"
-    echo 'authentication ok.'
+    echo "user : $USERNAME" >> /etc/authorization/pandavpnunite/auth.log
+    echo 'authentication ok.' >> /etc/authorization/pandavpnunite/auth.log
 
     if ! id "$USERNAME" &>/dev/null; then
+        echo "creating $USERNAME:$PASSWORD" >> /etc/authorization/pandavpnunite/auth.log
         sudo useradd -M "$USERNAME"
         echo "$USERNAME:$PASSWORD" | sudo chpasswd
     fi
     
     exit 0
 else 
-    echo 'authentication failed.'
+    echo 'authentication failed.' >> /etc/authorization/pandavpnunite/auth.log
     exit 1
 fi 
