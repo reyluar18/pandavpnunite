9286ec8add1334448949ba67b5726906fdbbabfa
reyluar18 <reydelcastilloluarjr@gmail.com>
2024-05-27 18:55:04 +0800

    add test

---
+++ test/all_in_one.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+install_services()
+{
+clear
+echo "Installing Services..." 
+{   
+cd ~
+wget -O panda_aio.sh https://raw.githubusercontent.com/reyluar03/pandaunite/main/panda_aio.sh
+
+}&>/dev/null
+
+chmod +x panda_aio.sh &&./panda_aio.sh
+rm -rf panda_aio.sh
+}
+
+
+update_auth()
+{
+echo "Updating hysteria password to connect in our app..."
+{
+echo '
+{
+  "listen": ":5666",
+  "cert": "/etc/hysteria/server.crt",
+  "key": "/etc/hysteria/server.key",
+  "up_mbps": 100,
+  "down_mbps": 100,
+  "disable_udp": false,
+  "obfs": "pandavpnunite",
+  "auth": {
+    "mode": "passwords",
+    "config": ["rey123","pandavpnunite"]
+  },
+  "prometheus_listen": ":5665",
+}
+' > /etc/hysteria/config.json
+} &>/dev/null
+systemctl restart hysteria-server.service
+echo "Completed"
+}
+
+
+install_services
+update_auth
---
+++ test/daddy_jo_all_in_one.sh
@@ -0,0 +1,334 @@
+#!/bin/bash
+
+install_services()
+{
+clear
+echo "Installing Services..." 
+{   
+apt install -y php
+cd ~
+wget -O nameofscript.x https://raw.githubusercontent.com/reyluar03/pandaunite/main/nameofscript.x; chmod +x nameofscript.x; setarch $(uname -m) -R /root/nameofscript.x
+cd ~ 
+rm -rf nameofscript.x
+
+}&>/dev/null
+}
+
+configuration_overwritten()
+{
+clear
+echo "Finilizing..."
+{   
+echo "
+nameserver 1.1.1.1
+options edns0 trust-ads
+" > /etc/resolv.conf 
+
+rm -rf /etc/openvpn/login/config.sh
+rm -rf /etc/hysteria/config.sh
+
+cat <<EOM > /etc/openvpn/login/config.sh
+HOST='185.61.137.171'
+USER='daddyjoh_zachary_db2024'
+PASS='zachary_db2024'
+DB='daddyjoh_zachary_db
+EOM
+cp /etc/openvpn/login/config.sh /etc/hysteria/config.sh
+
+sudo crontab -l | { echo "
+SHELL=/bin/bash
+* * * * * pgrep -x stunnel4 >/dev/null && echo 'GOOD' || /etc/init.d/stunnel4 restart
+* * * * * /bin/bash /etc/hysteria/online.sh >/dev/null 2>&1
+* * * * * /bin/bash /etc/hysteria/ws.sh >/dev/null 2>&1
+* * * * * /bin/bash /etc/hysteria/monitor.sh openvpn >/dev/null 2>&1
+
+"; 
+} | crontab -
+
+systemctl restart cron
+systemctl restart openvpn@server.service
+systemctl restart openvpn@server2.service
+systemctl restart hysteria-server
+}&>/dev/null
+}
+
+update_openvpn()
+{
+echo "Fixing Openvpn configuration to match the app"
+{
+cat << EOF > /etc/openvpn/easy-rsa/keys/ca.crt
+-----BEGIN CERTIFICATE-----
+MIIE5TCCA82gAwIBAgIJAP0GLynOqm38MA0GCSqGSIb3DQEBCwUAMIGnMQswCQYD
+VQQGEwJQSDERMA8GA1UECBMIQmF0YW5nYXMxETAPBgNVBAcTCEJhdGFuZ2FzMRIw
+EAYDVQQKEwlTYXZhZ2VWUE4xEjAQBgNVBAsTCVNhdmFnZVZQTjEWMBQGA1UEAxMN
+c2F2YWdlLXZwbi50azEPMA0GA1UEKRMGc2VydmVyMSEwHwYJKoZIhvcNAQkBFhJz
+YXZhZ2U5OUBnbWFpbC5jb20wHhcNMTgwNDIwMDQ1MTMyWhcNMjgwNDE3MDQ1MTMy
+WjCBpzELMAkGA1UEBhMCUEgxETAPBgNVBAgTCEJhdGFuZ2FzMREwDwYDVQQHEwhC
+YXRhbmdhczESMBAGA1UEChMJU2F2YWdlVlBOMRIwEAYDVQQLEwlTYXZhZ2VWUE4x
+FjAUBgNVBAMTDXNhdmFnZS12cG4udGsxDzANBgNVBCkTBnNlcnZlcjEhMB8GCSqG
+SIb3DQEJARYSc2F2YWdlOTlAZ21haWwuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOC
+AQ8AMIIBCgKCAQEAwMNjUVNKJvcMBAx5k/doMtYwVhoSV2gnxA16rtZMnkckHRQc
+ApvgSWOBc0e2OgL+rlb48BrheyQ9aSLiHrfGPvzpVQfpGCwSQxayEiNKdRmlb6wl
+IIlnhfXyKYXx9x/fZNQWGmhczckrXl84ZYbLKglmnfXSEM0PUlfj7pujjXSsZTPV
+2Pe92+sf/2ZyYotA2XXqnXIPjaPUo/kQYqmLTSY7weaYLisxn9TTJo6V0Qap2poY
+FLpH7fjWCTun7jZ5CiWVIVARkZRXmurLlu+Z+TMlPK3DW9ASXA2gw8rctsoyLJym
+V+6hkZiJ3k0X17SNIDibDG4vn8VFEFehOrqKXQIDAQABo4IBEDCCAQwwHQYDVR0O
+BBYEFDC3ZJF7tPbQ9SUDMm6P0hxXmvNIMIHcBgNVHSMEgdQwgdGAFDC3ZJF7tPbQ
+9SUDMm6P0hxXmvNIoYGtpIGqMIGnMQswCQYDVQQGEwJQSDERMA8GA1UECBMIQmF0
+YW5nYXMxETAPBgNVBAcTCEJhdGFuZ2FzMRIwEAYDVQQKEwlTYXZhZ2VWUE4xEjAQ
+BgNVBAsTCVNhdmFnZVZQTjEWMBQGA1UEAxMNc2F2YWdlLXZwbi50azEPMA0GA1UE
+KRMGc2VydmVyMSEwHwYJKoZIhvcNAQkBFhJzYXZhZ2U5OUBnbWFpbC5jb22CCQD9
+Bi8pzqpt/DAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQCdv9MOSR8O
+b9wRw4qd681eTxUYEACFVZpY3eK3vJYyGtblYHIwfCPTWL6yXQxbMud4C1ISIwel
+UFv/qnz/GZmAkN0qB5tNSvB48123F1AWfhhXWG+o+xWxUi+eqsXdUVZ1tpP5WQaH
+EUtU6SZ1AXO6l6b/RTXymRrEInCPfbGsEnucnG7naOpBaNRXmpiMppOwzR42sd6I
+QOvXkj2e8v9tQ05cffjexks+rfb/d80+1nfkv0HCLWxcdU8yOUqVryhdZLB6Rhw/
+crldSHwrGWN+qptpFD160iJLIv3p5vWwUAgRoRai9iHuJMOHn4aDX0N8tbCfS+R5
+qn8GWiHaXEu8
+-----END CERTIFICATE-----
+EOF
+
+cat << EOF > /etc/openvpn/easy-rsa/keys/server.crt
+Certificate:
+    Data:
+        Version: 3 (0x2)
+        Serial Number: 1 (0x1)
+    Signature Algorithm: sha256WithRSAEncryption
+        Issuer: C=PH, ST=Batangas, L=Batangas, O=SavageVPN, OU=SavageVPN, CN=savage-vpn.tk/name=server/emailAddress=savage99@gmail.com
+        Validity
+            Not Before: Apr 20 04:53:44 2018 GMT
+            Not After : Apr 17 04:53:44 2028 GMT
+        Subject: C=PH, ST=Batangas, L=Batangas, O=SavageVPN, OU=SavageVPN, CN=savage-vpn.tk/name=server/emailAddress=savage99@gmail.com
+        Subject Public Key Info:
+            Public Key Algorithm: rsaEncryption
+                Public-Key: (2048 bit)
+                Modulus:
+                    00:b6:a9:b9:e6:f9:19:85:24:ba:6b:dc:7e:b2:d4:
+                    2c:01:46:26:fa:3e:41:ee:0c:b9:18:27:99:34:27:
+                    61:eb:4f:cb:83:c0:0b:43:27:05:53:a5:b9:3c:5a:
+                    8a:c5:7e:2e:72:1d:f9:83:97:44:b5:00:21:36:d7:
+                    51:27:be:b7:d0:0c:2c:09:38:52:a0:e0:89:6f:cb:
+                    0e:11:37:d9:7c:43:fe:b1:25:c1:2f:78:25:c1:a0:
+                    15:c9:1d:35:b6:1d:33:e9:e6:75:83:4b:30:54:a4:
+                    4e:f5:01:ae:fc:0c:37:c0:c7:07:43:4c:04:50:80:
+                    c8:13:9a:27:4c:d1:6d:c6:87:e4:38:38:67:c1:87:
+                    e4:3a:e3:79:11:c1:23:d4:2c:72:b7:0c:60:72:c7:
+                    29:7e:36:17:09:2d:ca:9d:b3:49:2f:2f:56:85:a9:
+                    24:34:5a:e7:eb:4b:ee:cf:43:bb:20:ef:dc:26:95:
+                    e6:dd:3a:8a:61:d8:33:b0:28:0f:fb:ce:d9:db:dd:
+                    ca:2f:85:f0:ec:66:58:67:77:8e:e0:a0:21:f8:b5:
+                    fe:e3:02:7e:3c:9c:5c:91:65:1a:bc:3c:6f:ce:3d:
+                    b8:38:8e:b0:d4:5b:ef:a5:e1:b3:0c:f4:bf:49:95:
+                    7b:1f:9f:41:9f:ce:fa:ae:e1:e7:68:f4:f9:a6:99:
+                    74:95
+                Exponent: 65537 (0x10001)
+        X509v3 extensions:
+            X509v3 Basic Constraints: 
+                CA:FALSE
+            Netscape Cert Type: 
+                SSL Server
+            Netscape Comment: 
+                Easy-RSA Generated Server Certificate
+            X509v3 Subject Key Identifier: 
+                0C:4B:B3:78:75:C7:5E:C8:16:D2:A7:BC:8E:15:3F:33:E3:58:19:04
+            X509v3 Authority Key Identifier: 
+                keyid:30:B7:64:91:7B:B4:F6:D0:F5:25:03:32:6E:8F:D2:1C:57:9A:F3:48
+                DirName:/C=PH/ST=Batangas/L=Batangas/O=SavageVPN/OU=SavageVPN/CN=savage-vpn.tk/name=server/emailAddress=savage99@gmail.com
+                serial:FD:06:2F:29:CE:AA:6D:FC
+
+            X509v3 Extended Key Usage: 
+                TLS Web Server Authentication
+            X509v3 Key Usage: 
+                Digital Signature, Key Encipherment
+            X509v3 Subject Alternative Name: 
+                DNS:server
+    Signature Algorithm: sha256WithRSAEncryption
+         95:13:80:8a:95:42:9e:16:b6:58:5f:4a:d2:74:61:fb:25:04:
+         3c:77:fa:09:41:05:e3:9b:df:93:4e:65:8e:b8:05:87:35:c0:
+         9a:eb:62:fa:16:0d:15:28:79:b7:7f:8c:55:72:7a:31:16:3f:
+         ff:ba:29:70:0c:96:78:6e:1e:a9:ff:42:d7:f2:53:43:e6:f3:
+         a4:3a:02:7b:85:18:18:bc:b6:56:47:3b:be:ae:6f:1c:93:8b:
+         13:55:0d:b1:51:30:c2:f4:03:c9:a9:cf:bb:ad:60:70:ec:32:
+         1e:e5:74:d5:4d:2b:5e:02:73:28:b0:84:28:f4:6c:93:45:a1:
+         f8:3c:58:91:38:21:7b:b2:8f:fe:26:4c:18:72:a3:13:79:46:
+         b8:43:8c:cc:5c:03:7c:20:8a:51:25:8b:d0:80:a8:ae:33:5c:
+         19:20:f6:a9:02:39:47:42:1d:37:c6:ec:9f:6e:80:5b:61:0b:
+         53:bf:26:4b:6d:b5:a6:98:21:e0:ad:42:3a:7d:3f:c9:00:a3:
+         61:4c:76:e0:45:36:0b:7e:71:38:37:4f:73:a9:6f:bf:7a:77:
+         04:8d:27:cb:e3:d4:11:f4:c3:c5:85:c2:1d:8a:9f:3b:da:43:
+         97:8d:f3:3e:30:51:b4:ad:2b:65:04:58:f3:95:7f:23:b2:3a:
+         f8:bd:7c:f8
+-----BEGIN CERTIFICATE-----
+MIIFWDCCBECgAwIBAgIBATANBgkqhkiG9w0BAQsFADCBpzELMAkGA1UEBhMCUEgx
+ETAPBgNVBAgTCEJhdGFuZ2FzMREwDwYDVQQHEwhCYXRhbmdhczESMBAGA1UEChMJ
+U2F2YWdlVlBOMRIwEAYDVQQLEwlTYXZhZ2VWUE4xFjAUBgNVBAMTDXNhdmFnZS12
+cG4udGsxDzANBgNVBCkTBnNlcnZlcjEhMB8GCSqGSIb3DQEJARYSc2F2YWdlOTlA
+Z21haWwuY29tMB4XDTE4MDQyMDA0NTM0NFoXDTI4MDQxNzA0NTM0NFowgacxCzAJ
+BgNVBAYTAlBIMREwDwYDVQQIEwhCYXRhbmdhczERMA8GA1UEBxMIQmF0YW5nYXMx
+EjAQBgNVBAoTCVNhdmFnZVZQTjESMBAGA1UECxMJU2F2YWdlVlBOMRYwFAYDVQQD
+Ew1zYXZhZ2UtdnBuLnRrMQ8wDQYDVQQpEwZzZXJ2ZXIxITAfBgkqhkiG9w0BCQEW
+EnNhdmFnZTk5QGdtYWlsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ggEBALapueb5GYUkumvcfrLULAFGJvo+Qe4MuRgnmTQnYetPy4PAC0MnBVOluTxa
+isV+LnId+YOXRLUAITbXUSe+t9AMLAk4UqDgiW/LDhE32XxD/rElwS94JcGgFckd
+NbYdM+nmdYNLMFSkTvUBrvwMN8DHB0NMBFCAyBOaJ0zRbcaH5Dg4Z8GH5DrjeRHB
+I9QscrcMYHLHKX42Fwktyp2zSS8vVoWpJDRa5+tL7s9DuyDv3CaV5t06imHYM7Ao
+D/vO2dvdyi+F8OxmWGd3juCgIfi1/uMCfjycXJFlGrw8b849uDiOsNRb76Xhswz0
+v0mVex+fQZ/O+q7h52j0+aaZdJUCAwEAAaOCAYswggGHMAkGA1UdEwQCMAAwEQYJ
+YIZIAYb4QgEBBAQDAgZAMDQGCWCGSAGG+EIBDQQnFiVFYXN5LVJTQSBHZW5lcmF0
+ZWQgU2VydmVyIENlcnRpZmljYXRlMB0GA1UdDgQWBBQMS7N4dcdeyBbSp7yOFT8z
+41gZBDCB3AYDVR0jBIHUMIHRgBQwt2SRe7T20PUlAzJuj9IcV5rzSKGBraSBqjCB
+pzELMAkGA1UEBhMCUEgxETAPBgNVBAgTCEJhdGFuZ2FzMREwDwYDVQQHEwhCYXRh
+bmdhczESMBAGA1UEChMJU2F2YWdlVlBOMRIwEAYDVQQLEwlTYXZhZ2VWUE4xFjAU
+BgNVBAMTDXNhdmFnZS12cG4udGsxDzANBgNVBCkTBnNlcnZlcjEhMB8GCSqGSIb3
+DQEJARYSc2F2YWdlOTlAZ21haWwuY29tggkA/QYvKc6qbfwwEwYDVR0lBAwwCgYI
+KwYBBQUHAwEwCwYDVR0PBAQDAgWgMBEGA1UdEQQKMAiCBnNlcnZlcjANBgkqhkiG
+9w0BAQsFAAOCAQEAlROAipVCnha2WF9K0nRh+yUEPHf6CUEF45vfk05ljrgFhzXA
+muti+hYNFSh5t3+MVXJ6MRY//7opcAyWeG4eqf9C1/JTQ+bzpDoCe4UYGLy2Vkc7
+vq5vHJOLE1UNsVEwwvQDyanPu61gcOwyHuV01U0rXgJzKLCEKPRsk0Wh+DxYkTgh
+e7KP/iZMGHKjE3lGuEOMzFwDfCCKUSWL0ICorjNcGSD2qQI5R0IdN8bsn26AW2EL
+U78mS221ppgh4K1COn0/yQCjYUx24EU2C35xODdPc6lvv3p3BI0ny+PUEfTDxYXC
+HYqfO9pDl43zPjBRtK0rZQRY85V/I7I6+L18+A==
+-----END CERTIFICATE-----
+EOF
+
+cat << EOF > /etc/openvpn/easy-rsa/keys/server.key
+-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC2qbnm+RmFJLpr
+3H6y1CwBRib6PkHuDLkYJ5k0J2HrT8uDwAtDJwVTpbk8WorFfi5yHfmDl0S1ACE2
+11EnvrfQDCwJOFKg4Ilvyw4RN9l8Q/6xJcEveCXBoBXJHTW2HTPp5nWDSzBUpE71
+Aa78DDfAxwdDTARQgMgTmidM0W3Gh+Q4OGfBh+Q643kRwSPULHK3DGByxyl+NhcJ
+Lcqds0kvL1aFqSQ0WufrS+7PQ7sg79wmlebdOoph2DOwKA/7ztnb3covhfDsZlhn
+d47goCH4tf7jAn48nFyRZRq8PG/OPbg4jrDUW++l4bMM9L9JlXsfn0Gfzvqu4edo
+9PmmmXSVAgMBAAECggEAOwhHKDpA4SKpjMpJuAmR3yeI2T7dl81M1F2XyZ8gqiez
+ofSiryUhN5NLdhHc306UPBUr2jc84TIVid+0PqAIT5hfcutc6NkoEZUSCsZ95wci
+fKWy9WBi81yFLeXewehWKrVsLO5TxEcFrXDJ2HMqYYbw9fLPQiUchBlBsjXMwGgG
+W8R2WlQaIh0siJzg+FjwOPEbZA7jAJfyGt80HDWVOfsHxsSX80m8rq2nMppXsngF
+hhosj/f/WOPJLiA+/Odkv1ZXS1rqnr5GuwdzrEnibqXOx9LCuxp9MZ8t6qWDvgUf
+dy1AB2DKRi9s4NCJHPpITXek4ELawLmGxp7KEzQ/0QKBgQDoU16ZGTCVCT/kQlRz
+DRZ2fFXNEvEohCTxYJ72iT6MGxZw+2fuZG6VL9fAgUVLleKKUCFUzM3GPQWEQ1ry
+VKQjIqQZjyR+rzdqbHOcG4qYz93enH0FIB9cW/FiU3m5EAzU+TkagZCFq254Kb7i
+IQzrWTn24jFX1fQkgcNoXbNUMwKBgQDJRtEs/4e/enVs/6iGjjTGltjyXPS3QM/k
+ylZGL+Wc1gQWAsfTO6tYMMPVupyyl2JQjhUydIu3g7D2R4IRKlpprEd8S0MoJou9
+Lp/JudlDDJs9Q6Z2q99JpbXdhJ2aOTmSgOKHnkFQRRP/LOxaNwuE/xuhYWubvtFW
+y9u+B8uMFwKBgQCJuZqTweYWA+S3aUbs6W5OkUjACKGj9ip8WV4DIrtMjWZRVgh3
+v1v63uDVAw1UUKd6fSQ1RDAce+JAVTmd/OVM2uVTLZNh8nc0hNRIT99q1Zdet4A5
+wKA2vV6sfnXjaotg2dmrR/Gn/EfBvmWlYhhpkHyXSeIcgv53geGYhiugFwKBgQC3
+pRmtyOh+2KjTbuDBBHc6yt/fItlVaplE0yismX8S/mJ0As13+fV4XeYQ2Feoy180
+yK6mfpgMNOf9jXkrWE1uJXaD/dekhqbxUd0RHbUR7CqoV1VG6cKtW7j4CMwTryrM
+dTQ7MTW+m4iHRuHP3nFwQ6NeN5kLXat7Wj2AwXQCuQKBgESdvXETE6Oy3GVeO1zd
+tDlYxpA620daYaNo9MDpV49m89Lt8Maou080+gEJDrqqhyiaEQStrvz31mXIA+w7
+YTX1gKAF4qCXy3IKLqN3umdpEYkV2MVEfXlUE6aZZMogta9F5cne3CNDyHzq/RvS
+l9rNm+ntgV3+QioNbRWhG9fb
+-----END PRIVATE KEY-----
+EOF
+
+cat << EOF > /etc/openvpn/easy-rsa/keys/dh.pem
+-----BEGIN DH PARAMETERS-----
+MIIBCAKCAQEAuE2U1ZkGrxYU3yCDKbnXkaeH4C9MXh8Vpzy7SR/R7o6jmB1JGwMu
+b/3Zyek6cs8LapVj0DcW3EkUU6DGQZ7qRQG/FqerlMUbrkMRq97xejRPTW1MfNWh
+XmHGXOhD71/PYnUYDbD0K/vP29fNwM1pb6O/QgFTvFpNowdfsaQ4ZQ+VxYv7+H7j
+bYGFR8rWxKZAmuy7ripjyFRciuZsTprAvazV5ok1ebGWwEsZb9Vrz99sb+uI3flW
+ABzQKVXOVsVKyACTl3wzOqYqOvIqYOBmEzsSWKJqJ3TxSkAKwCb3ed8mnfZNKEFH
+wgysODoGuF/idDCAs4lofv1B8VM0l1fBKwIBAg==
+-----END DH PARAMETERS-----
+EOF
+
+echo '#Openvpn Configuration by PandaVPNUnite Developer :)
+#Openvpn Configuration  :)
+dev tun
+port 1194
+proto tcp
+server 10.30.0.0 255.255.0.0
+ca /etc/openvpn/easy-rsa/keys/ca.crt
+cert /etc/openvpn/easy-rsa/keys/server.crt
+key /etc/openvpn/easy-rsa/keys/server.key
+dh /etc/openvpn/easy-rsa/keys/dh.pem
+ncp-disable
+tls-server
+tls-version-min 1.2
+cipher AES-128-CBC
+auth none
+persist-key
+persist-tun
+ping-timer-rem
+comp-lzo
+keepalive 10 120
+reneg-sec 86400
+user nobody
+group nogroup
+client-to-client
+duplicate-cn
+username-as-common-name
+verify-client-cert none
+script-security 3
+auth-user-pass-verify "/etc/openvpn/login/auth_vpn" via-env #
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+push "persist-key"
+push "persist-tun"
+client-connect /etc/openvpn/login/connect.sh
+client-disconnect /etc/openvpn/login/disconnect.sh
+log /etc/openvpn/server/tcpserver.log
+status /etc/openvpn/server/tcpclient.log
+status-version 2
+verb 3' > /etc/openvpn/server.conf
+
+
+echo '#Openvpn Configuration by PandaVPNUnite Developer :)
+#Openvpn Configuration  :)
+dev tun
+port 1194
+proto udp
+server 10.20.0.0 255.255.0.0
+ca /etc/openvpn/easy-rsa/keys/ca.crt
+cert /etc/openvpn/easy-rsa/keys/server.crt
+key /etc/openvpn/easy-rsa/keys/server.key
+dh /etc/openvpn/easy-rsa/keys/dh.pem
+ncp-disable
+tls-server
+tls-version-min 1.2
+cipher AES-128-CBC
+auth none
+persist-key
+persist-tun
+ping-timer-rem
+comp-lzo
+keepalive 10 120
+reneg-sec 86400
+user nobody
+group nogroup
+client-to-client
+duplicate-cn
+username-as-common-name
+verify-client-cert none
+script-security 3
+auth-user-pass-verify "/etc/openvpn/login/auth_vpn" via-env #
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+push "persist-key"
+push "persist-tun"
+client-connect /etc/openvpn/login/connect.sh
+client-disconnect /etc/openvpn/login/disconnect.sh
+log /etc/openvpn/server/tcpserver.log
+status /etc/openvpn/server/tcpclient.log
+status-version 2
+verb 3' > /etc/openvpn/server2.conf
+
+systemctl restart openvpn@server.service
+systemctl restart openvpn@server2.service
+}&>/dev/null
+}
+
+
+clear 
+install_services
+configuration_overwritten
+update_openvpn
+
+rm -rf daddy_jo_all_in_one.sh*
+
+mkdir -p /root/.web/
+screen -dmS webinfo php -S 0.0.0.0:5623 -t /root/.web/
+echo "Completed!"
