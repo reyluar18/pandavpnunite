dd150a7858f7427eb57fc853bcda6ab8ebee44cb
reyluar18 <reydelcastilloluarjr@gmail.com>
2024-06-09 08:08:16 +0800

    socks

--- websocket_3.py
+++ websocket_3.py
@@ -19,7 +19,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:442'
-RESPONSE = 'HTTP/1.1 101 <font color="red">Panda VPN Unite</font>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <font color="red">Socks Via SSH/SSL/Dropbear By Panda VPN Unite</font>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
